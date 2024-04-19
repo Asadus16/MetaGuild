72273ed861528d1ddc0d6ad0f6281af18c6e49a0
Asadus16 <asadslayer999@gmail.com>
2024-04-19 15:24:07 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,94 +1,35 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+import { ConnectWallet } from '@thirdweb-dev/react';
+import './styles/Home.css';
+import logo from '../src/images/logo.png';
+import Lottie from 'lottie-react';
+import Animation from './animation.json';
 
 export default function Home() {
   return (
-    <main className="main">
-      <div className="container">
-        <div className="header">
-          <h1 className="title">
-            Welcome to{" "}
-            <span className="gradient-text-0">
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className="description">
-            Get started by configuring your desired network in{" "}
-            <code className="code">src/index.js</code>, then modify the{" "}
-            <code className="code">src/App.js</code> file!
-          </p>
-
-          <div className="connect">
-            <ConnectWallet />
-          </div>
-        </div>
-
-        <div className="grid">
-          <a
-            href="https://portal.thirdweb.com/"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-1">Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-2">Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-3">Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
+    <>
+      <main className="main">
+        <a href="#">
+          <img src={logo} className="logo" />
+        </a>
+        <ConnectWallet className="connect" />
+      </main>
+      <div className="description">
+        <h1>A platform where every Contributor gets a Chance!</h1>
+        <p>
+          Create bounties for contributors, let contributors build their<br></br> web3 profile and pay with your own DAO token.
+        </p>
       </div>
-    </main>
+      <div className="animationDiv">
+        <Lottie animationData={Animation} className="animation" />
+      </div>
+      <div className="buttons">
+        <a href="#">
+          <button className="button1">Create Project</button>
+        </a>
+        <a href="#">
+          <button className="button2">Explore DAOs</button>
+        </a>
+      </div>
+    </>
   );
 }
