d2c0c7491dd82c3b2719b18557c2be2ce0749458
Ali Raza <ali@Alis-MacBook-Air.local>
2024-05-02 16:16:04 +0500

    aliiii

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,73 @@
-import { ConnectWallet } from '@thirdweb-dev/react';
 import logo from '../images/logo.png';
+// import { ConnectWallet } from '@thirdweb-dev/react';
+import Gif from '../images/bitcoin.gif';
+import Logo1 from '../images/Logo1.png';
+
+import {
+  ThirdwebProvider,
+  ConnectButton,
+  
+} from "thirdweb/react";
+import {
+  createWallet,
+  walletConnect,
+} from "thirdweb/wallets";
+import { createThirdwebClient } from "thirdweb";
+
+
+
+const ConnectWallet = () => {
+  
+  const client = createThirdwebClient({
+    clientId: "YOUR_CLIENT_ID",
+  });
+  
+  const wallets = [
+    walletConnect(),
+    createWallet("io.metamask"),
+    createWallet("com.coinbase.wallet"),
+    createWallet("com.trustwallet.app"),
+    createWallet("app.phantom"),
+  ];
+    return (
+      <ThirdwebProvider>
+        <ConnectButton
+          client={client}
+          wallets={wallets}
+          theme={"dark"}
+          connectModal={{
+            size: "wide",
+            titleIcon: Logo1,
+            title: "MetaGuild",
+            welcomeScreen: {
+              title:
+                "MetaGuild streamline your DAO's workflow!",
+              subtitle:
+                "Connect a wallet to get started",
+              img: {
+                src: Gif,
+                width: 200,
+                height: 200,
+              },
+            },
+          }}
+        />
+      </ThirdwebProvider>
+    );
+  }
+
 
 export default function Navbar() {
   return (
+    
     <main className="main">
       <a href="#">
         <img src={logo} className="logo" />
       </a>
-      <ConnectWallet className="connect" />
+      <div className="connect">
+      <ConnectWallet />
+      </div >
     </main>
   );
 }
+
--- src/pages/ExploreDAO.js
+++ src/pages/ExploreDAO.js
@@ -6,34 +6,29 @@ import DaoCard from '../Components/DaoCard';
 
 
 const SearchInput = () => {
-  const [isEditing, setIsEditing] = useState(true);
-  const [searchText, setsearchText] = useState('');
-
-  const handleEditClick = () => {
-    setIsEditing(true);
-  };
+  const [searchText, setSearchText] = useState('');
 
   const handleChange = (event) => {
-    setsearchText(event.target.value);
+    setSearchText(event.target.value);
+  };
+
+  const clearSearchText = () => {
+    setSearchText('');
   };
 
   return (
     <div className="search-container">
-      {isEditing ? (
-        <input 
+      <input 
         type="text" 
         value={searchText} 
         onChange={handleChange} 
         placeholder="Search DAOs..."
-        />
-      ) : (
-        <div onClick={handleEditClick} style={{ cursor: 'text' }}>
-          {searchText || 'Search DAOs...'}  {/* Display placeholder if empty */}
-        </div>
+      />
+      {searchText && (
+         <span className="edit-icon" onClick={clearSearchText}>
+          x
+        </span>
       )}
-      <span className="edit-icon" onClick={handleEditClick}>
-        x
-      </span>
     </div>
   );
 };
